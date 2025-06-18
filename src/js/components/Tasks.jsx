624b77ef50bc2cfc18a6289e412062dcca81381b
steph-mos <stephymosquera@gmail.com>
2025-06-18 14:35:47 +0000

    todolist react acabado

---
+++ src/js/components/Tasks.jsx
@@ -0,0 +1,63 @@
+import React, {useState} from 'react';
+
+const Tasks = () => {
+
+    const [tasksList, setTasksList] = useState ([]) 
+    const [newTask,setNewTask] = useState('')
+    
+    function addTask(){
+        
+        setTasksList([...tasksList,newTask]);
+        
+    }
+
+    function enterDone(e){
+        if (e.key == 'Enter'){
+            setTasksList([...tasksList,newTask]);
+            setNewTask('');
+        }
+
+    }
+
+    const deletTask = (index) => {
+        
+        setTasksList(tasksList.filter((_, i) => i !== index));
+    
+    }
+
+    return (
+        <>
+        <div className="container-fluid todolist">
+            <h1>todos</h1>
+            
+            <input 
+            type="text"
+            name="task" 
+            placeholder="What needs to be done?" 
+            vale={newTask} 
+            onChange={(e) => setNewTask(e.target.value)} 
+            onKeyDown={enterDone} />
+            
+            <ul>
+
+                {tasksList.map((task, index) => 
+                <li className="items md-auto" key={index}>
+                    <span>{task}</span>
+                    <button className="delete-button" onClick={()=> deletTask(index)} >x</button>
+                </li>)}
+
+                {/* <li><button onClick={addTask} >Add to do</button></li> */}
+
+                <li><p className="itemsleft">{tasksList.length}items left</p></li>
+            </ul>
+         
+            
+            
+        </div>
+        </>
+    );
+
+};
+
+
+export default Tasks 
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -10,9 +10,10 @@ import '../styles/index.css'
 
 // components
 import Home from './components/Home';
+import Tasks from './components/Tasks';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <Tasks/>
   </React.StrictMode>,
 )
